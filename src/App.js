47f2d3206fd067ab97f687debad2f0f22a7a40be
Eerika <e2203061@edu.vamk.fi>
2024-01-19 15:37:39 +0200

    Updated navbar

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import { Link , Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
 import Home from './components/Home';
+import Calendar from './components/Calendar';
+import Booking from './components/Booking';
 import Doctor from './components/Doctor';
 import Petowner from './components/Petowner';
 
@@ -75,6 +77,8 @@ function App() {
       <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
         <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
           <li><Link to="/" className="nav-link px-2 text-white">Home</Link></li>
+          <li><Link to="booking/" className="nav-link px-2 text-white">Booking</Link></li>
+          <li><Link to="/calendar" className="nav-link px-2 text-white">Calendar</Link></li>
           </ul>
 
         <div className="text-end">
@@ -83,11 +87,10 @@ function App() {
       </div>
     </div>
   </header>
-
-  
-
     <Routes>
       <Route path='/' element={<Home />}/>
+      <Route path='/booking' element={<Booking />}/>
+      <Route path='/calendar' element={<Calendar />}/>
       <Route path="/login" element={<Login />}/>
     </Routes>
     </div>
@@ -96,25 +99,3 @@ function App() {
 };
 
 export default App;
-
-
-
-
-
-/*
-import Nav from "./Navbar"
-
-const App = () => {
-  return (
-    <div className="App">
-      <Nav />
-      <Routes>
-        <Route path="/" element={ <HomePage/> } />
-        <Route path="booking" element={ <BookingPage/> } />
-        <Route path="calendar" element={ <CalendarPage/> } />
-      </Routes>
-      
-    </div>
-  )
-}
- */
